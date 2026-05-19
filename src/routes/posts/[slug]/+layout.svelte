e4d2189d85b54231cdb1a11f1af8c50074ea661d
Amadu Swaray <amaduswaray06@gmail.com>
2026-05-19 22:35:39 +0200

    muted blog text color

--- src/routes/posts/[slug]/+layout.svelte
+++ src/routes/posts/[slug]/+layout.svelte
@@ -48,7 +48,7 @@
 
 	<hr class="dark:border-border my-6 border-neutral-200" />
 
-	<div class="prose dark:prose-invert dark:text-foreground text-justify text-lg text-neutral-800">
+	<div class="prose dark:prose-invert text-foreground/80 text-justify text-base">
 		{@render children()}
 	</div>
 </article>
